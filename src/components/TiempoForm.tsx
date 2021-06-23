9afe5c3d91d9e98b246a998b7d702ca7c9e5d17d
jhonny <jhorguma@gmail.com>
2021-06-22 19:13:37 -0500

    prototipo api consulta

---
+++ src/components/TiempoForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+conts TiempForm = props =>(
+    <div className='card card-body'>
+        <form action="" method="post">
+            <div className='form-group'>
+                <input type="text" name='city' className='form-control' autoFocus/>
+            </div>
+        </form>
+    </div>
+)
---
+++ src/components/TiempoInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+conts TiempoInfo = props =>{
+    return(
+        <div>
+            Tiempo informacion
+        </div>
+    )
+}
+
+export default TiempoInfo;
